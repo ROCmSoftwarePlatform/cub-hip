e2b8a6cc2a61918057f33982899bdcc0aec5bf34
Nagamani Nagaraj <nagamani@multicorewareinc.com>
2016-12-30 11:51:48 +0530

    Use __attribuite__(aligned(16)) for 16 bytes alignment in hccbackend

--- cub/block/block_radix_rank.cuh
+++ cub/block/block_radix_rank.cuh
@@ -58,7 +58,7 @@ namespace cub {
  * \tparam DESCENDING           Whether or not the sorted-order is high-to-low
  * \tparam MEMOIZE_OUTER_SCAN   <b>[optional]</b> Whether or not to buffer outer raking scan partials to incur fewer shared memory reads at the expense of higher register pressure (default: true for architectures SM35 and newer, false otherwise).  See BlockScanAlgorithm::BLOCK_SCAN_RAKING_MEMOIZE for more details.
  * \tparam INNER_SCAN_ALGORITHM <b>[optional]</b> The cub::BlockScanAlgorithm algorithm to use (default: cub::BLOCK_SCAN_WARP_SCANS)
- * \tparam SMEM_CONFIG          <b>[optional]</b> Shared memory bank mode (default: \p cudaSharedMemBankSizeFourByte)
+ * \tparam SMEM_CONFIG          <b>[optional]</b> Shared memory bank mode (default: \p hipSharedMemBankSizeFourByte)
  * \tparam BLOCK_DIM_Y          <b>[optional]</b> The thread block length in threads along the Y dimension (default: 1)
  * \tparam BLOCK_DIM_Z          <b>[optional]</b> The thread block length in threads along the Z dimension (default: 1)
  * \tparam PTX_ARCH             <b>[optional]</b> \ptxversion
@@ -89,7 +89,7 @@ template <
     bool                    DESCENDING,
     bool                    MEMOIZE_OUTER_SCAN      = (CUB_PTX_ARCH >= 350) ? true : false,
     BlockScanAlgorithm      INNER_SCAN_ALGORITHM    = BLOCK_SCAN_WARP_SCANS,
-    cudaSharedMemConfig     SMEM_CONFIG             = cudaSharedMemBankSizeFourByte,
+    hipSharedMemConfig     SMEM_CONFIG             = hipSharedMemBankSizeFourByte,
     int                     BLOCK_DIM_Y             = 1,
     int                     BLOCK_DIM_Z             = 1,
     int                     PTX_ARCH                = CUB_PTX_ARCH>
@@ -105,7 +105,7 @@ private:
     typedef unsigned short DigitCounter;
 
     // Integer type for packing DigitCounters into columns of shared memory banks
-    typedef typename If<(SMEM_CONFIG == cudaSharedMemBankSizeEightByte),
+    typedef typename If<(SMEM_CONFIG == hipSharedMemBankSizeEightByte),
         unsigned long long,
         unsigned int>::Type PackedCounter;
 
@@ -150,6 +150,7 @@ private:
 
 
     /// Shared memory storage layout type for BlockRadixRank
+#ifdef __HIP_PLATFORM_NVCC__
     struct __align__(16) _TempStorage
     {
         union
@@ -162,7 +163,19 @@ private:
         typename BlockScan::TempStorage block_scan;
     };
 
+#elif defined(__HIP_PLATFORM_HCC__)
+    struct __attribute__((aligned(16))) _TempStorage
+    {
+        union
+        {
+            DigitCounter            digit_counters[PADDED_COUNTER_LANES][BLOCK_THREADS][PACKING_RATIO];
+            PackedCounter           raking_grid[BLOCK_THREADS][RAKING_SEGMENT];
+        };
 
+        // Storage for scanning local ranks
+        typename BlockScan::TempStorage block_scan;
+    };
+#endif
     /******************************************************************************
      * Thread fields
      ******************************************************************************/
