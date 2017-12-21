44f89e70314cf7b8a5b1cc746820407b21970c91
Neelakandan <neelakandan@multicorewareinc.com>
2017-12-21 19:08:01 +0530

    Enable CUB overload operators for vector types

--- test/test_util.h
+++ test/test_util.h
@@ -1037,7 +1037,6 @@ std::ostream& operator<<(std::ostream& os, const cub::KeyValuePair<Key, Value> &
 /**
  * Define for types
  */
-#ifdef __HIP_PLATFORM_NVCC__ //Below macros conflict with hip_vector_types ones: TODO:NEEL
 CUB_VEC_OVERLOAD(char, char)
 CUB_VEC_OVERLOAD(short, short)
 CUB_VEC_OVERLOAD(int, int)
@@ -1050,7 +1049,6 @@ CUB_VEC_OVERLOAD(ulong, unsigned long)
 CUB_VEC_OVERLOAD(ulonglong, unsigned long long)
 CUB_VEC_OVERLOAD(float, float)
 CUB_VEC_OVERLOAD(double, double)
-#endif
 
 
 //---------------------------------------------------------------------
